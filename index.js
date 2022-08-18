0a127a7a4c6c02427ce996b00c33daea3f31636d
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-08-18 22:18:36 +0100

    function added

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 let input = document.querySelector("#input-field");
-
 maxLength = input.getAttribute("maxlength");
-
 counter = document.querySelector(".holder .limiter");
-
-
 let cLimiter = counter.innerText;
-
-
 function handlekey() {
     cLimiter = Number(maxLength) - input.value.length;
     counter.innerText = cLimiter;
